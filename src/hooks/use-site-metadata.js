d8aba8ba4553c210e780f1418d1a081b8c93feeb
ggdaltoso <gabriel.daltoso@catho.com>
2019-05-26 15:52:36 -0300

    blog start

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,38 @@
+// @flow
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            author {
+              name
+              bio
+              photo
+              contacts {
+                email
+                twitter
+                github
+              }
+            }
+            menu {
+              label
+              path
+            }
+            url
+            title
+            subtitle
+            copyright
+            disqusShortname
+          }
+        }
+      }
+    `,
+  );
+
+  return site.siteMetadata;
+};
+
+export default useSiteMetadata;
