b246f47b717465bfdbc908ff292c697c98a59f04
ggdaltoso <ggdaltoso@gmail.com>
2020-07-17 09:21:33 -0300

    feat(posts): add mdx render support

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,16 +1,27 @@
 // @flow
-import React from 'react';
-import styles from './Content.module.scss';
+import React from "react";
+import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer";
+
+import styles from "./Content.module.scss";
 
 type Props = {
   body: string,
-  title: string
+  title: string,
 };
 
-const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+const Content = ({ body, title, mdx }: Props) => (
+  <div className={styles["content"]}>
+    <h1 className={styles["content__title"]}>{title}</h1>
+    {mdx ? (
+      <div className={styles["content__body"]}>
+        <MDXRenderer>{body}</MDXRenderer>
+      </div>
+    ) : (
+      <div
+        className={styles["content__body"]}
+        dangerouslySetInnerHTML={{ __html: body }}
+      />
+    )}
   </div>
 );
 
