266b652eaa86ec51459272e6a4054580e113d17c
ggdaltoso <ggdaltoso@gmail.com>
2020-07-17 09:20:57 -0300

    chore(posts): add mdx support to post-template

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import Post from '../components/Post';
-import { useSiteMetadata } from '../hooks';
-import type { MarkdownRemark } from '../types';
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { useSiteMetadata } from "../hooks";
+import type { MarkdownRemark } from "../types";
 
 type Props = {
-  data: MarkdownRemark
+  data: MarkdownRemark,
 };
 
 const PostTemplate = ({ data }: Props) => {
   const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
-  const { title: postTitle, description: postDescription } = data.markdownRemark.frontmatter;
-  const metaDescription = postDescription !== null ? postDescription : siteSubtitle;
+  const {
+    title: postTitle,
+    description: postDescription,
+  } = data.markdownRemark.frontmatter;
+  const metaDescription =
+    postDescription !== null ? postDescription : siteSubtitle;
 
   return (
     <Layout title={`${postTitle} - ${siteTitle}`} description={metaDescription}>
-      <Post post={data.markdownRemark} />
+      <Post post={data.markdownRemark} mdx={data.mdx} />
     </Layout>
   );
 };
-
 
 export const query = graphql`
   query PostBySlug($slug: String!) {
@@ -38,8 +41,10 @@ export const query = graphql`
         title
       }
     }
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      body
+    }
   }
 `;
-
 
 export default PostTemplate;
