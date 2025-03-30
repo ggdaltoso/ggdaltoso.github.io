c696327801b08db85ad0064ec1d50cf651aeaf02
ggdaltoso <ggdaltoso@gmail.com>
2025-03-30 10:57:13 -0300

    feat: initialize project with Astro framework, add global configurations, and create initial blog structure

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Blog do GG';
+export const SITE_DESCRIPTION = 'O que fiz, o que faço e o que vou fazer';
