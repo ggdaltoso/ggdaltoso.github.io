75a3c4e59d4f5a57e811ef3ad738d7d619217275
ggdaltoso <ggdaltoso@gmail.com>
2025-04-18 14:35:32 -0300

    feat: rename blog collection to posts for consistency

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const posts = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
@@ -16,4 +16,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { posts };
