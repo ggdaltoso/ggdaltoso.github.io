1b7bc89541e6b974e7f15be04ad586f86ff836f3
ggdaltoso <ggdaltoso@gmail.com>
2025-12-23 16:24:25 -0300

    chore: clean up Highlight and Image components
    - Removed unused styled components and imports from Highlight.js.
    - Simplified GGImage component by replacing custom title bar with TitleBar from @react95/core.

--- src/components/Layout/Highlight.js
+++ src/components/Layout/Highlight.js
@@ -1,39 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
 import { Highlight, themes } from 'prism-react-renderer';
 import { Frame } from '@react95/core';
-
-const Wrapper = styled.div`
-  font-family: sans-serif;
-  text-align: center;
-`;
-
-const Pre = styled.pre`
-  text-align: left;
-  margin: 0;
-  padding: 0.5em;
-
-  & .token-line {
-    line-height: 1.3em;
-    height: 1.3em;
-  }
-`;
-
-const Line = styled.div`
-  display: table-row;
-`;
-
-const LineNo = styled.span`
-  display: table-cell;
-  text-align: right;
-  padding-right: 1em;
-  user-select: none;
-  opacity: 0.5;
-`;
-
-const LineContent = styled.span`
-  display: table-cell;
-`;
 
 const GGHighlight = (props) => {
   return (
--- src/components/Layout/Image.js
+++ src/components/Layout/Image.js
@@ -1,43 +1,14 @@
 import React from 'react';
-import { Frame } from '@react95/core';
-import Icons, { Wangimg128 } from '@react95/icons';
-import styled from 'styled-components';
-
-const Title = styled.span`
-  color: white;
-  margin: 0 0 0 2px;
-  font-size: 0.675rem;
-  line-height: 0.875rem;
-  display: flex;
-  align-items: center;
-  font-weight: bold;
-`;
+import { Frame, TitleBar } from '@react95/core';
+import { Wangimg128 } from '@react95/icons';
 
 const GGImage = (props) => {
   const { src, alt, title, width } = props;
 
   return (
     <Frame p={1} width={`${width}px`} mh="auto">
-      <Frame
-        bg="headerBackground"
-        height={18}
-        w="100%"
-        boxShadow="none"
-        pl={2}
-        pt={1}
-        display="flex"
-      >
-        <Wangimg128
-          variant="16x16_4"
-          style={{
-            width: 15,
-            height: 15,
-            maxWidth: 'unset',
-            margin: 0,
-          }}
-        />
-        <Title>{title}</Title>
-      </Frame>
+      <TitleBar title={title} icon={<Wangimg128 variant="16x16_4" />} />
+
       <Frame p={3} pr={4} as="figure" boxShadow="none">
         <Frame bg="white" boxShadow="in" pt={2} pl={2}>
           <img src={src} alt={alt} title={title} />
