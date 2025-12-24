b6ac6bb078b7f8881907d62158124bce9d753890
ggdaltoso <ggdaltoso@gmail.com>
2025-12-24 10:16:38 -0300

    chore: remove unused styles from Layout component
    Removed unnecessary imports for '@react95/core/themes/win95.css' and '@react95/core/GlobalStyle' from the Layout component to clean up the codebase.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,9 +6,6 @@ import GGHighlight from './Highlight';
 import GGImage from './Image';
 import * as styles from './Layout.module.scss';
 
-import '@react95/core/themes/win95.css';
-import '@react95/core/GlobalStyle';
-
 const Layout = ({ children, title, description }) => (
   <div className={styles.layout}>
     <Helmet>
