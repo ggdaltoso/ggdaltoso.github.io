9eed306d941e0cedb7d4d1d75c1e21999fcc384b
ggdaltoso <ggdaltoso@gmail.com>
2025-12-24 16:26:06 -0300

    chore: remove gatsby-plugin-mdx configuration
    This change removes the gatsby-plugin-mdx configuration from gatsby-config.js to streamline the setup and eliminate unnecessary dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,25 +92,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: { maxWidth: 960, showCaptions: true },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
-          },
-          'gatsby-remark-autolink-headers',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-        extensions: ['.md'],
-      },
-    },
-    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
