56a2941d87b2e19ca5bc4b146b551921b19b7d69
ggdaltoso <ggdaltoso@gmail.com>
2026-03-12 07:27:45 -0300

    fix(metadata): update author contacts to replace Twitter with Bluesky
    Updated the site metadata to reflect the change from Twitter to Bluesky for author contacts. This ensures accurate representation of the author's social media links.

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -1,34 +1,32 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
 const useSiteMetadata = () => {
-  const { site } = useStaticQuery(
-    graphql`
-      query SiteMetaData {
-        site {
-          siteMetadata {
-            author {
-              name
-              bio
-              photo
-              contacts {
-                email
-                twitter
-                github
-              }
+  const { site } = useStaticQuery(graphql`
+    query SiteMetaData {
+      site {
+        siteMetadata {
+          author {
+            name
+            bio
+            photo
+            contacts {
+              email
+              bluesky
+              github
             }
-            menu {
-              label
-              path
-            }
-            url
-            title
-            subtitle
-            copyright
           }
+          menu {
+            label
+            path
+          }
+          url
+          title
+          subtitle
+          copyright
         }
       }
-    `,
-  );
+    }
+  `);
 
   return site.siteMetadata;
 };
