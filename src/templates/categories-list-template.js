26ad9823a046f5f53f39324e35e1d9b2096ba4eb
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:02:58 -0300

    feat(i18n): enhance categories list template for internationalization
    - Updated CategoriesListTemplate to utilize i18next for translations.
    - Integrated locale paths for category links to support multiple languages.
    - Improved subtitle localization and page title translation.

--- src/templates/categories-list-template.js
+++ src/templates/categories-list-template.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import kebabCase from 'lodash/kebabCase';
 import Sidebar from '../components/Sidebar';
 import Layout from '../components/Layout';
 import Page from '../components/Page';
-import { useSiteMetadata, useCategoriesList } from '../hooks';
+import { useSiteMetadata } from '../hooks';
+import { getLocalePaths, getLocalizedValue, withLocalePath } from '../utils';
 
-const CategoriesListTemplate = () => {
+const CategoriesListTemplate = ({ data, pageContext }) => {
   const { title, subtitle } = useSiteMetadata();
-  const categories = useCategoriesList();
+  const { language, defaultLanguage } = useI18next();
+  const localizedSubtitle = getLocalizedValue(subtitle, language, defaultLanguage);
+  const { t } = useTranslation();
+  const { locale } = pageContext;
+  const localePaths = getLocalePaths(data.site.siteMetadata, locale);
+  const categories = data.allMarkdownRemark.group;
 
   return (
-    <Layout title={`Categories - ${title}`} description={subtitle}>
+    <Layout title={`${t('Categories')} - ${title}`} description={localizedSubtitle}>
       <Sidebar />
-      <Page title="Categories">
+      <Page title={t('Categories')}>
         <ul>
           {categories.map((category) => (
             <li key={category.fieldValue}>
-              <Link to={`/category/${kebabCase(category.fieldValue)}/`}>
+              <Link
+                to={withLocalePath(
+                  `/${localePaths.category}/${kebabCase(category.fieldValue)}/`,
+                  locale,
+                  data.site.siteMetadata.i18n.defaultLocale,
+                )}
+              >
                 {category.fieldValue} ({category.totalCount})
               </Link>
             </li>
@@ -27,5 +40,36 @@ const CategoriesListTemplate = () => {
     </Layout>
   );
 };
+
+export const query = graphql`
+  query CategoriesListTemplate($locale: String!) {
+    site {
+      siteMetadata {
+        i18n {
+          defaultLocale
+          paths {
+            pt {
+              category
+            }
+            en {
+              category
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { template: { eq: "post" }, draft: { ne: true } }
+        fields: { locale: { eq: $locale } }
+      }
+    ) {
+      group(field: { frontmatter: { category: SELECT } }) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
 
 export default CategoriesListTemplate;
