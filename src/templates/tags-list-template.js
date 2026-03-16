5cdb3d1782bc03616c41a806c3fc6928153cd586
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:03:29 -0300

    feat(i18n): enhance tags list template for internationalization support
    - Updated TagsListTemplate to utilize internationalization features.
    - Integrated translation for titles and subtitles using useTranslation.
    - Adjusted tag links to support locale-specific paths.

--- src/templates/tags-list-template.js
+++ src/templates/tags-list-template.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import kebabCase from 'lodash/kebabCase';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Page from '../components/Page';
-import { useSiteMetadata, useTagsList } from '../hooks';
+import { useSiteMetadata } from '../hooks';
+import { getLocalePaths, getLocalizedValue, withLocalePath } from '../utils';
 
-const TagsListTemplate = () => {
+const TagsListTemplate = ({ data, pageContext }) => {
   const { title, subtitle } = useSiteMetadata();
-  const tags = useTagsList();
+  const { language, defaultLanguage } = useI18next();
+  const localizedSubtitle = getLocalizedValue(subtitle, language, defaultLanguage);
+  const { t } = useTranslation();
+  const { locale } = pageContext;
+  const localePaths = getLocalePaths(data.site.siteMetadata, locale);
+  const tags = data.allMarkdownRemark.group;
 
   return (
-    <Layout title={`Tags - ${title}`} description={subtitle}>
+    <Layout title={`${t('Tags')} - ${title}`} description={localizedSubtitle}>
       <Sidebar />
-      <Page title="Tags">
+      <Page title={t('Tags')}>
         <ul>
           {tags.map((tag) => (
             <li key={tag.fieldValue}>
-              <Link to={`/tag/${kebabCase(tag.fieldValue)}/`}>
+              <Link
+                to={withLocalePath(
+                  `/${localePaths.tags}/${kebabCase(tag.fieldValue)}/`,
+                  locale,
+                  data.site.siteMetadata.i18n.defaultLocale,
+                )}
+              >
                 {tag.fieldValue} ({tag.totalCount})
               </Link>
             </li>
@@ -27,5 +40,36 @@ const TagsListTemplate = () => {
     </Layout>
   );
 };
+
+export const query = graphql`
+  query TagsListTemplate($locale: String!) {
+    site {
+      siteMetadata {
+        i18n {
+          defaultLocale
+          paths {
+            pt {
+              tags
+            }
+            en {
+              tags
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { template: { eq: "post" }, draft: { ne: true } }
+        fields: { locale: { eq: $locale } }
+      }
+    ) {
+      group(field: { frontmatter: { tags: SELECT } }) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
 
 export default TagsListTemplate;
