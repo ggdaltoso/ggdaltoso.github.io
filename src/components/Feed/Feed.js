375854d92094ab62b84809bc16ca6a90cefd4905
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:05:07 -0300

    feat(i18n): integrate translation support in Feed component
    - Added useTranslation hook to enable internationalization.
    - Updated category and title links to support translated text.
    - Enhanced the read more link to utilize translated content.

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,49 +1,56 @@
 import React from 'react';
 import moment from 'moment';
 import { Link } from 'gatsby';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 import * as styles from './Feed.module.scss';
 
-const Feed = ({ edges }) => (
-  <div>
-    {edges.map((edge) => (
-      <div className={styles['feed__item']} key={edge.node.fields.slug}>
-        <div>
-          <time
-            className={styles['feed__itemMetaTime']}
-            dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}
-          >
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
-          </time>
-          <span className={styles['feed__itemMetaDivider']} />
-          <span>
-            <Link
-              to={edge.node.fields.categorySlug}
-              className={styles['feed__itemMetaCategoryLink']}
+const Feed = ({ edges }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      {edges.map((edge) => (
+        <div className={styles['feed__item']} key={edge.node.fields.slug}>
+          <div>
+            <time
+              className={styles['feed__itemMetaTime']}
+              dateTime={moment(edge.node.frontmatter.date).format(
+                'MMMM D, YYYY',
+              )}
             >
-              {edge.node.frontmatter.category}
+              {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+            </time>
+            <span className={styles['feed__itemMetaDivider']} />
+            <span>
+              <Link
+                to={edge.node.fields.categorySlug}
+                className={styles['feed__itemMetaCategoryLink']}
+              >
+                {edge.node.frontmatter.category}
+              </Link>
+            </span>
+          </div>
+          <h2 className={styles['feed__itemTitle']}>
+            <Link
+              className={styles['feed__itemTitleLink']}
+              to={edge.node.fields.slug}
+            >
+              {edge.node.frontmatter.title}
             </Link>
-          </span>
-        </div>
-        <h2 className={styles['feed__itemTitle']}>
+          </h2>
+          <p className={styles['feed__itemDescription']}>
+            {edge.node.frontmatter.description}
+          </p>
           <Link
-            className={styles['feed__itemTitleLink']}
+            className={styles['feed__itemReadmore']}
             to={edge.node.fields.slug}
           >
-            {edge.node.frontmatter.title}
+            {t('Read')}
           </Link>
-        </h2>
-        <p className={styles['feed__itemDescription']}>
-          {edge.node.frontmatter.description}
-        </p>
-        <Link
-          className={styles['feed__itemReadmore']}
-          to={edge.node.fields.slug}
-        >
-          Read
-        </Link>
-      </div>
-    ))}
-  </div>
-);
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Feed;
