a60c625fbf7b246fc7a6ad7f961f917757910cef
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:02:53 -0300

    feat(i18n): enhance index template for internationalization support
    - Integrated useI18next for language handling.
    - Added getLocalizedValue utility for subtitle localization.
    - Updated GraphQL query to include locale filtering for posts.

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Page from '../components/Page';
 import Pagination from '../components/Pagination';
 import { useSiteMetadata } from '../hooks';
+import { getLocalizedValue } from '../utils';
 
 const IndexTemplate = ({ data, pageContext }) => {
-  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
+  const { title: siteTitle, subtitle } = useSiteMetadata();
+  const { language, defaultLanguage } = useI18next();
+  const siteSubtitle = getLocalizedValue(subtitle, language, defaultLanguage);
 
   const {
     currentPage,
@@ -39,11 +43,23 @@ const IndexTemplate = ({ data, pageContext }) => {
 };
 
 export const query = graphql`
-  query IndexTemplate($postsLimit: Int!, $postsOffset: Int!) {
+  query IndexTemplate($locale: String!, $postsLimit: Int!, $postsOffset: Int!) {
+    locales: allLocale(filter: { language: { eq: $locale } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
     allMarkdownRemark(
       limit: $postsLimit
       skip: $postsOffset
-      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+      filter: {
+        frontmatter: { template: { eq: "post" }, draft: { ne: true } }
+        fields: { locale: { eq: $locale } }
+      }
       sort: { frontmatter: { date: DESC } }
     ) {
       edges {
