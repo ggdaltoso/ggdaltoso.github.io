f28dbf331dfa849c71afbda6925c4af177b7930a
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:05:30 -0300

    feat(i18n): add localized author bio support
    Updated the Author component to utilize localization for the author's biography. This change integrates the use of the `useI18next` hook to fetch the appropriate language and default language, ensuring that the author's bio is displayed in the correct language based on user preferences.

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { getContactHref } from '../../../utils';
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import { getContactHref, getLocalizedValue } from '../../../utils';
 import * as styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
 const Author = () => {
   const { author } = useSiteMetadata();
+  const { language, defaultLanguage } = useI18next();
+  const authorBio = getLocalizedValue(author.bio, language, defaultLanguage);
 
   return (
     <div className={styles['author']}>
       <p>
-        {author.bio}
+        {authorBio}
         <a
           className={styles['author__bioBluesky']}
           href={getContactHref('bluesky', author.contacts.bluesky)}
