2d2a4fa609fe12308b5fb0f1613aacf548dfe84a
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:03:09 -0300

    feat(i18n): enhance post template for internationalization support
    - Integrated i18next for language handling.
    - Updated site subtitle localization using getLocalizedValue.
    - Modified GraphQL query to support locale-based data fetching.

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 import Layout from '../components/Layout';
 import Post from '../components/Post';
 import { useSiteMetadata } from '../hooks';
+import { getLocalizedValue } from '../utils';
 
 const PostTemplate = ({ data }) => {
-  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
+  const { title: siteTitle, subtitle } = useSiteMetadata();
+  const { language, defaultLanguage } = useI18next();
+  const siteSubtitle = getLocalizedValue(subtitle, language, defaultLanguage);
   const { html: postBody } = data.markdownRemark;
   const { title: postTitle, description: postDescription } =
     data.markdownRemark.frontmatter;
@@ -20,8 +24,17 @@ const PostTemplate = ({ data }) => {
 };
 
 export const query = graphql`
-  query PostBySlug($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
+  query PostById($id: String!, $locale: String!) {
+    locales: allLocale(filter: { language: { eq: $locale } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
       id
       html
       fields {
