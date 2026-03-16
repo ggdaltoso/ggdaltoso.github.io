c958d6fb6e6490ed84d8e793fb75771a8c55231f
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:05:16 -0300

    feat(i18n): add language support in Layout component
    - Integrated useI18next hook to dynamically set the language attribute in the HTML tag.
    - Enhanced internationalization support for the Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import * as styles from './Layout.module.scss';
 import 'react-medium-image-zoom/dist/styles.css';
+import { useI18next } from 'gatsby-plugin-react-i18next';
 
-const Layout = ({ children, title, description }) => (
-  <div className={styles.layout}>
-    <Helmet>
-      <html lang="en" />
-      <title>{title}</title>
-      <meta name="description" content={description} />
-    </Helmet>
-    {children}
-  </div>
-);
+const Layout = ({ children, title, description }) => {
+  const { language } = useI18next();
+
+  return (
+    <div className={styles.layout}>
+      <Helmet>
+        <html lang={language} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
