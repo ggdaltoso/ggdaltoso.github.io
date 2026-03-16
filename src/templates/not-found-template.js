87301c84c54c8c578a54ca4ecf12cd02c06044fb
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:03:03 -0300

    feat(i18n): enhance NotFoundTemplate for internationalization support
    - Integrated i18next for language handling.
    - Added localized title and description for the not found page.
    - Improved subtitle localization using getLocalizedValue utility.

--- src/templates/not-found-template.js
+++ src/templates/not-found-template.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import Sidebar from '../components/Sidebar';
 import Layout from '../components/Layout';
 import Page from '../components/Page';
 import { useSiteMetadata } from '../hooks';
+import { getLocalizedValue } from '../utils';
 
 const NotFoundTemplate = () => {
   const { title, subtitle } = useSiteMetadata();
+  const { language, defaultLanguage } = useI18next();
+  const { t } = useTranslation();
+  const localizedSubtitle = getLocalizedValue(subtitle, language, defaultLanguage);
+
+  const notFoundTitle = t('Not found');
+  const notFoundDescription =
+    language === 'en'
+      ? 'The route you tried to access does not exist.'
+      : 'A rota que voce tentou acessar nao existe.';
 
   return (
-    <Layout title={`Not Found - ${title}`} description={subtitle}>
+    <Layout title={`${notFoundTitle} - ${title}`} description={localizedSubtitle}>
       <Sidebar />
-      <Page title="NOT FOUND">
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Page title={notFoundTitle}>
+        <p>{notFoundDescription}</p>
       </Page>
     </Layout>
   );
