22b8b169732dcdec72c0ee801f3c042b95904a13
ggdaltoso <ggdaltoso@gmail.com>
2026-03-16 14:05:59 -0300

    feat(i18n): add LanguageSwitcher component for language selection
    - Implemented LanguageSwitcher component to allow users to select their preferred language.
    - Added corresponding styles in LanguageSwitcher.module.scss for layout and active state.
    - Updated index.js to export the LanguageSwitcher component.

---
+++ src/components/Sidebar/LanguageSwitcher/LanguageSwitcher.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, useI18next } from 'gatsby-plugin-react-i18next';
+import * as styles from './LanguageSwitcher.module.scss';
+
+const LanguageSwitcher = () => {
+  const { languages, language, originalPath } = useI18next();
+
+  return (
+    <nav className={styles['languageSwitcher']} aria-label="Language selector">
+      <ul className={styles['languageSwitcher__list']}>
+        {languages.map((lng) => {
+          const isActive = lng === language;
+
+          return (
+            <li className={styles['languageSwitcher__listItem']} key={lng}>
+              <Link
+                to={originalPath}
+                language={lng}
+                className={`${styles['languageSwitcher__listItemLink']} ${
+                  isActive ? styles['languageSwitcher__listItemLinkActive'] : ''
+                }`}
+              >
+                {lng.toUpperCase()}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default LanguageSwitcher;
