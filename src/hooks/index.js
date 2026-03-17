47cee772595fb695b4a61023b721283ef8cf51df
ggdaltoso <ggdaltoso@gmail.com>
2026-03-17 14:26:14 -0300

    feat(i18n): export useLocalizedValue hook in index.js
    Added the useLocalizedValue hook to the index.js file to facilitate localized value retrieval across the application.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useSiteMetadata } from './use-site-metadata';
 export { default as useLocalizedSiteMetadata } from './use-localized-site-metadata';
+export { default as useLocalizedValue } from './use-localized-value';
 export { default as useLanguageSwitcher } from './use-language-switcher';
