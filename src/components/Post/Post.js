14771aea28d75c2fc4f19413df4fcb095f18933f
ggdaltoso <ggdaltoso@gmail.com>
2026-03-17 13:55:29 -0300

    chore(i18n): remove Tags component and associated styles
    This commit removes the Tags component and its related styles from the Post component, streamlining the codebase by eliminating unused features.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,13 +5,12 @@ import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
 import Meta from './Meta';
-import Tags from './Tags';
 import * as styles from './Post.module.scss';
 
 const Post = ({ post, html }) => {
   const { t } = useTranslation();
-  const { tagSlugs, slug } = post.fields || {};
-  const { tags, title, date } = post.frontmatter;
+  const { slug } = post.fields || {};
+  const { title, date } = post.frontmatter;
 
   return (
     <div className={styles['post']}>
@@ -25,7 +24,6 @@ const Post = ({ post, html }) => {
 
       <div className={styles['post__footer']}>
         <Meta date={date} />
-        {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
         <Author />
       </div>
 
