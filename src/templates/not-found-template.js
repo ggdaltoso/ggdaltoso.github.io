e5c083c40c1178434e03482b1a16a10969706e5a
ggdaltoso <ggdaltoso@gmail.com>
2026-03-17 16:34:46 -0300

    feat: update NotFoundTemplate to remove localized subtitle and integrate SEO component
    - Removed the use of localizedSubtitle from the NotFoundTemplate.
    - Integrated the SEO component for better document head management.

--- src/templates/not-found-template.js
+++ src/templates/not-found-template.js
@@ -3,11 +3,11 @@ import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import Sidebar from '../components/Sidebar';
 import Layout from '../components/Layout';
 import Page from '../components/Page';
-import { useLocalizedSiteMetadata } from '../hooks';
+import SEO from '../components/SEO';
+import siteConfig from '../../config.js';
 import { buildDocumentTitle } from '../utils';
 
 const NotFoundTemplate = () => {
-  const { localizedSubtitle } = useLocalizedSiteMetadata();
   const { language } = useI18next();
   const { t } = useTranslation();
 
@@ -18,15 +18,29 @@ const NotFoundTemplate = () => {
       : 'A rota que voce tentou acessar nao existe.';
 
   return (
-    <Layout
-      title={buildDocumentTitle(notFoundTitle)}
-      description={localizedSubtitle}
-    >
+    <Layout>
       <Sidebar />
       <Page title={notFoundTitle}>
         <p>{notFoundDescription}</p>
       </Page>
     </Layout>
+  );
+};
+
+export const Head = ({ pageContext }) => {
+  const locale = pageContext.locale || siteConfig.i18n?.defaultLocale || 'pt';
+  const notFoundTitle = locale === 'en' ? 'Not found' : 'Pagina nao encontrada';
+  const notFoundDescription =
+    locale === 'en'
+      ? 'The route you tried to access does not exist.'
+      : 'A rota que voce tentou acessar nao existe.';
+
+  return (
+    <SEO
+      locale={locale}
+      title={buildDocumentTitle(notFoundTitle)}
+      description={notFoundDescription}
+    />
   );
 };
 
