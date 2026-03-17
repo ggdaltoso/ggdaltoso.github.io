9b9d7fd4c4ca1fd00e7196a1d70e4176d6ed0254
ggdaltoso <ggdaltoso@gmail.com>
2026-03-17 16:34:31 -0300

    feat: remove react-helmet integration from Layout component
    This commit removes the react-helmet dependency and its usage in the Layout component, simplifying the component structure and eliminating unnecessary imports.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 import * as styles from './Layout.module.scss';
 import 'react-medium-image-zoom/dist/styles.css';
-import { useI18next } from 'gatsby-plugin-react-i18next';
 
-const Layout = ({ children, title, description }) => {
-  const { language } = useI18next();
-
-  return (
-    <div className={styles.layout}>
-      <Helmet>
-        <html lang={language} />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-      </Helmet>
-      {children}
-    </div>
-  );
-};
+const Layout = ({ children }) => <div className={styles.layout}>{children}</div>;
 
 export default Layout;
