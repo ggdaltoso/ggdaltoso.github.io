582baf7fca708d467a3f7b11a2516636e22004ba
ggdaltoso <ggdaltoso@gmail.com>
2026-03-18 08:45:33 -0300

    feat: refactor Meta component to use date-fns for date formatting
    Replaced moment.js with date-fns for improved date handling and localization in the Meta component. This change enhances the performance and reduces the bundle size by eliminating the dependency on moment.js.

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import moment from 'moment';
+import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
+import { formatLocalizedDate } from '../../../utils';
 import * as styles from './Meta.module.scss';
 
-const Meta = ({ date }) => (
-  <div>
-    <p className={styles['meta__date']}>
-      Published {moment(date).format('D MMM YYYY')}
-    </p>
-  </div>
-);
+const Meta = ({ date }) => {
+  const { language } = useI18next();
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <p className={styles['meta__date']}>
+        {t('Published')} {formatLocalizedDate(date, 'd MMM yyyy', language)}
+      </p>
+    </div>
+  );
+};
 
 export default Meta;
