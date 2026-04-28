405d9d2c0f3178a8b1da78e68f7ba1d747367c6d
ggdaltoso <ggdaltoso@gmail.com>
2026-04-28 08:37:30 -0300

    feat: add import for @react95/fonts in Layout component
    This change introduces the import of the @react95/fonts package in the Layout component to ensure that the necessary font styles are applied throughout the application.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as styles from './Layout.module.scss';
 import 'react-medium-image-zoom/dist/styles.css';
+import '@react95/fonts';
 
 const Layout = ({ children }) => <div className={styles.layout}>{children}</div>;
 
